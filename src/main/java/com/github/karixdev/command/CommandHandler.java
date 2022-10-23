1e2098e02e83fa723fa7b74cc9678cc9afb0f598
karixdev <kari.klucz@gmail.com>
2022-10-23 23:25:37 +0200

    Created method that informs if command was not found

--- src/main/java/com/github/karixdev/command/CommandHandler.java
+++ src/main/java/com/github/karixdev/command/CommandHandler.java
@@ -2,6 +2,7 @@ package com.github.karixdev.command;
 
 import com.github.karixdev.account.AccountService;
 import com.github.karixdev.database.Database;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.*;
@@ -11,9 +12,6 @@ public class CommandHandler {
     public final static char COMMAND_PREFIX = '!';
 
     private final AccountService accountService;
-
-    private final Map<String, ICommand> commandMap = Map.of();
-
     public CommandHandler(AccountService accountService) {
         this.accountService = accountService;
     }
@@ -22,7 +20,11 @@ public class CommandHandler {
         String command = args[0].substring(1);
 
         List<String> params = Arrays.stream(args).filter(arg -> !arg.equals(command)).toList();
+    }
 
-
+    private void sendCommandNotFoundMessage(Message message) {
+        message
+                .reply("Command was not found")
+                .queue();
     }
 }
