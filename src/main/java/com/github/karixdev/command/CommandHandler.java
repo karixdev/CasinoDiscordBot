327c6217aa750d482d81773a472575e22dd5cbd4
karixdev <kari.klucz@gmail.com>
2022-10-23 17:17:35 +0200

    Created CommandHandlerClass

---
+++ src/main/java/com/github/karixdev/command/CommandHandler.java
@@ -0,0 +1,9 @@
+package com.github.karixdev.command;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class CommandHandler {
+    public void handle(String[] args, MessageReceivedEvent event) {
+
+    }
+}
--- src/main/java/com/github/karixdev/discordbot/DiscordMessageListener.java
+++ src/main/java/com/github/karixdev/discordbot/DiscordMessageListener.java
@@ -1,15 +1,33 @@
 package com.github.karixdev.discordbot;
 
+import com.github.karixdev.command.CommandHandler;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Arrays;
+
 public class DiscordMessageListener extends ListenerAdapter {
+
+    CommandHandler commandHandler = new CommandHandler();
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) {
             return;
         }
 
-        event.getMessage().reply("Your id: " + event.getAuthor().getId()).queue();
+        String[] args = event.getMessage().getContentRaw()
+                .toLowerCase()
+                .split("\\s+");
+
+        if (isMessageACommand(args[0])) {
+            commandHandler.handle(args, event);
+        }
+    }
+
+    private boolean isMessageACommand(String message) {
+        return message.charAt(0) == '!';
     }
 }
