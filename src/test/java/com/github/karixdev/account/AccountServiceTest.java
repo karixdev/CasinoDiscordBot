cd6e90563fd0af1135c6c5346640918ca7eb71a7
karixdev <kari.klucz@gmail.com>
2022-10-23 19:49:00 +0200

    Created test for updateCredits

--- src/test/java/com/github/karixdev/account/AccountServiceTest.java
+++ src/test/java/com/github/karixdev/account/AccountServiceTest.java
@@ -1,10 +1,14 @@
 package com.github.karixdev.account;
 
 import com.github.karixdev.TestDatabase;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AccountServiceTest {
     private AccountService accountService;
@@ -21,6 +25,16 @@ public class AccountServiceTest {
 
     @Test
     public void GivenDiscordId_WhenCreateAccount_ThenDoesNotThrowSqlException() {
-        Assertions.assertDoesNotThrow(() -> accountService.createAccount(11));
+        assertDoesNotThrow(() -> accountService.createAccount(11));
+    }
+
+    @Test
+    public void GivenDiscordId_WhenUpdateCredits_ThenReturnsValidCredits() throws SQLException {
+        int id = 11;
+        accountService.createAccount(id);
+
+        int result = accountService.updateCredits(id, 1237);
+
+        assertEquals(1237, result);
     }
 }
