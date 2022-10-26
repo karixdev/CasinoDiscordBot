2c2a07094b57897e4045d81dfa0568591cdfa998
karixdev <kari.klucz@gmail.com>
2022-10-26 14:46:05 +0200

    Created and implemented CoinFlipCommand

---
+++ src/main/java/com/github/karixdev/game/coinflip/CoinFlipCommand.java
@@ -0,0 +1,47 @@
+package com.github.karixdev.game.coinflip;
+
+import com.github.karixdev.account.Account;
+import com.github.karixdev.account.AccountService;
+import com.github.karixdev.game.BaseGameCommand;
+import com.github.karixdev.game.GameMessagesUtils;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class CoinFlipCommand extends BaseGameCommand {
+    public CoinFlipCommand(AccountService accountService) {
+        super(accountService);
+    }
+
+    @Override
+    protected String expectedInput() {
+        return "heads/tails credits. Credits must be greater than 0.";
+    }
+
+    @Override
+    protected void play(MessageReceivedEvent event, String param, int credits, Account account) {
+        String[] options = {"heads", "tails"};
+
+        if (!Arrays.asList(options).contains(param)) {
+            GameMessagesUtils.sendInvalidParamsMessage(event.getMessage(), expectedInput());
+            return;
+        }
+
+        Random random = new Random();
+        int winner = Math.abs(random.nextInt() % 2);
+
+        int newCredits = account.getCredits();
+
+        if (options[winner].equals(param)) {
+            GameMessagesUtils.sendWinMessage(event.getChannel(), event.getAuthor(), credits);
+            newCredits += credits;
+        } else {
+            GameMessagesUtils.sendLossMessage(event.getChannel(), event.getAuthor(), credits);
+            newCredits -= credits;
+        }
+
+        account.setCredits(newCredits);
+    }
+}
