fef7dd074b41b3a4f9e652760d07475a1bf1b80a
karixdev <kari.klucz@gmail.com>
2022-10-26 20:48:20 +0200

    Implemented RussianRouletteCommand play method

--- src/main/java/com/github/karixdev/game/russianroulette/RussianRouletteCommand.java
+++ src/main/java/com/github/karixdev/game/russianroulette/RussianRouletteCommand.java
@@ -6,6 +6,8 @@ import com.github.karixdev.game.BaseGameCommand;
 import com.github.karixdev.game.GameCommandValidator;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import java.util.Random;
+
 public class RussianRouletteCommand extends BaseGameCommand {
     public RussianRouletteCommand(AccountService accountService) {
         super(accountService);
@@ -13,12 +15,24 @@ public class RussianRouletteCommand extends BaseGameCommand {
 
     @Override
     protected String expectedInput() {
-        return "shots_num credits. There are 6 bullets in chamber. Credits must be greater than 0.";
+        return "shots_num credits. There are 6 bullets in chamber, so you can choose between 1 and 5. Credits must be greater than 0.";
     }
 
     @Override
     protected void play(MessageReceivedEvent event, String param, int credits, Account account) {
+        int shots = Integer.parseInt(param);
 
+
+        Random random = new Random();
+        int bulletPositionInChamber = random.nextInt(1, 6) + 1;
+
+        int expectedCredits = shots * credits;
+
+        if (shots < bulletPositionInChamber) {
+            account.setCredits(account.getCredits() + expectedCredits);
+        } else {
+            account.setCredits(account.getCredits() - expectedCredits);
+        }
     }
 
     @Override
---
+++ src/main/java/com/github/karixdev/game/russianroulette/RussianRouletteCommandFactory.java
@@ -0,0 +1,19 @@
+package com.github.karixdev.game.russianroulette;
+
+import com.github.karixdev.account.AccountService;
+import com.github.karixdev.command.CommandFactory;
+import com.github.karixdev.command.ICommand;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.List;
+
+public class RussianRouletteCommandFactory extends CommandFactory {
+    public RussianRouletteCommandFactory(AccountService accountService) {
+        super(accountService);
+    }
+
+    @Override
+    protected ICommand createCommand(AccountService accountService, MessageReceivedEvent event, List<String> params) {
+        return new RussianRouletteCommand(accountService);
+    }
+}
