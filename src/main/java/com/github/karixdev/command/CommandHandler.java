a3a151cb34a1db0ca8dac6a63499cba3560f367c
karixdev <kari.klucz@gmail.com>
2022-10-27 16:42:42 +0200

    Moved all game commands handling to CommandHandler

--- src/main/java/com/github/karixdev/command/CommandHandler.java
+++ src/main/java/com/github/karixdev/command/CommandHandler.java
@@ -2,9 +2,8 @@ package com.github.karixdev.command;
 
 import com.github.karixdev.account.AccountCommandFactory;
 import com.github.karixdev.account.AccountService;
-import com.github.karixdev.database.Database;
-import com.github.karixdev.game.GameCommandFactoryManager;
 import com.github.karixdev.game.coinflip.CoinFlipCommandFactory;
+import com.github.karixdev.game.russianroulette.RussianRouletteCommandFactory;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
@@ -15,6 +14,7 @@ public class CommandHandler {
     public final static char COMMAND_PREFIX = '!';
 
     private final AccountService accountService;
+
     public CommandHandler(AccountService accountService) {
         this.accountService = accountService;
     }
@@ -30,14 +30,12 @@ public class CommandHandler {
     }
 
     public void executeCertainCommand(String command, List<String> params, MessageReceivedEvent event) {
-        CommandFactory commandFactory = null;
-
-        if (command.equals("account")) {
-            commandFactory = new AccountCommandFactory(accountService);
-        } else if (command.equals("game")) {
-            GameCommandFactoryManager gameCommandFactoryManager = new GameCommandFactoryManager(accountService);
-            commandFactory = gameCommandFactoryManager.chooseFactory(event.getMessage(), params);
-        }
+        CommandFactory commandFactory = switch (command) {
+            case "account" -> new AccountCommandFactory(accountService);
+            case "coin-flip" -> new CoinFlipCommandFactory(accountService);
+            case "russian" -> new RussianRouletteCommandFactory(accountService);
+            default -> null;
+        };
 
         if (commandFactory == null) {
             sendCommandNotFoundMessage(event.getMessage());
